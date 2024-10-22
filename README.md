245eae6db353be0884f18e83ea112fee8500cca3
Sumanthh007 <150594401+Sumanthh007@users.noreply.github.com>
2024-10-22 20:23:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Self-ordering-kiosk
+# Self-Ordering Kiosk System
+
+## Overview
+
+Welcome to the Self-Ordering Kiosk System! This system is designed to streamline the ordering process for customers and facilitate efficient order management for kitchen staff. It includes two main components:
+
+1. **Order Screen**: Allows customers to browse the menu, place orders, and view their order status.
+2. **Kitchen Screen**: Enables kitchen staff to review incoming orders, mark them as ready or delivered, and manage the overall order flow.
+
+## Features
+
+### Order Screen
+
+- **Menu Display**: Showcases a visually appealing and interactive menu with categories and item details.
+- **Order Placement**: Customers can select items, customize their choices, and add them to their cart.
+- **Order Summary**: Displays a summary of the selected items, allowing customers to review and modify their orders before finalizing.
+- **Payment Integration**: Facilitates secure payment options, including credit/debit card transactions and digital wallets.
+- **Order Tracking**: Provides real-time updates on the order status
+
+### Kitchen Screen
+
+- **Order Queue**: Displays a list of incoming orders with details such as items ordered, customer preferences, and order time.
+- **Order Review**: Allows kitchen staff to view detailed information about each order and make necessary adjustments.
+- **Status Management**: Enables staff to mark orders as 'Ready' or 'Delivered,' helping track the progress of each order.
+- **Notifications**: Alerts staff to new orders and updates to existing orders, ensuring timely preparation and delivery.
+
+ http://localhost:5000/api/products/seed
---
+++ data.js
@@ -0,0 +1,83 @@
+const data = {
+  categories: [
+    {
+      name: 'Beverages',
+      image: '/images/beverages.jpg',
+    },
+    {
+      name: 'Breakfast',
+      image: '/images/breakfast.jpg',
+    },
+    {
+      name: 'Burgers',
+      image: '/images/burgers.jpg',
+    },
+  ],
+  products: [
+    {
+      category: 'Beverages',
+      name: 'Coca-Cola',
+      calorie: 120,
+      price: 40,
+      image: '/images/t-mcdonalds-Coca-Cola-Classic-Small.jpg',
+    },
+    {
+      category: 'Beverages',
+      name: 'Vanilla Shake',
+      price: 60,
+      calorie: 360,
+      image: '/images/t-mcdonalds-Vanilla-McCafe-Shake-Medium.jpg',
+    },
+    {
+      category: 'Beverages',
+      name: 'Hot Chocolate',
+      price: 80,
+      calorie: 170,
+      image: '/images/t-mcdonalds-McCafe-Hot-Chocolate-Medium.jpg',
+    },
+    {
+      category: 'Breakfast',
+      name: 'Bacon & Biscuit',
+      price: 140,
+      calorie: 90,
+      image: '/images/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit.jpg',
+    },
+    {
+      category: 'Breakfast',
+      name: 'Blueberry Muffin',
+      price: 200,
+      calorie: 120,
+      image: '/images/t-blueberry-muffin.jpg',
+    },
+    {
+      category: 'Breakfast',
+      name: 'Big Breakfast',
+      price: 300,
+      calorie: 430,
+      image: '/images/s-mcdonalds-Big-Breakfast-Regular-Size-Biscuit.jpg',
+    },
+    {
+      category: 'Burgers',
+      name: 'Big Mac',
+      price: 190,
+      calorie: 200,
+      image: '/images/t-mcdonalds-Big-Mac.jpg',
+    },
+    {
+      category: 'Burgers',
+      name: 'Hamburger',
+      price: 120,
+      calorie: 410,
+      image: '/images/t-mcdonalds-Hamburger.jpg',
+    },
+    {
+      category: 'Burgers',
+      name: 'McDouble',
+      price: 230,
+      calorie: 320,
+      image: '/images/t-mcdonalds-McDouble.jpg',
+    },
+  ],
+};
+
+module.exports = data;
